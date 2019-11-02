5d941912d48914008dbedbe05b31d8e5d7d6a97b
Strojj <scepkamichal1@gmail.com>
2019-11-02 20:30:01 +0100

    threads default

---
+++ Threads/threads-demo.cpp
@@ -0,0 +1,280 @@
+// ***********************************************************************
+//
+// Demo program for subject Computer Architectures and Paralel systems 
+// Petr Olivka, Dept. of Computer Science, FEECS, VSB-TU Ostrava
+// email:petr.olivka@vsb.cz
+//
+// Threads programming example for Linux (10/2016)
+// For the propper testing is necessary to have at least 2 cores CPU
+//
+// ***********************************************************************
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/param.h>
+#include <pthread.h>
+
+#define TYPE int
+
+class task_part
+{
+public:
+	int id;                 // user identification
+	int first, last, N;     // data range
+	TYPE* data;             // array
+	TYPE max = 0;           // result
+	TYPE* result;   	    // result*
+
+	task_part(int myid, int first, int last, TYPE* ptr)
+	{
+		this->id = myid;
+		this->first = first;
+		this->last = last;
+		this->data = ptr;
+        this->N = last - first;
+        this->result = fillArray();
+	}
+
+	TYPE get_max() { return max; }
+
+	TYPE* get_result() { return result; }
+
+	// function search_max search the largest number in part of array
+	// from the left (included) up to the right element
+	TYPE search_max()
+	{
+		TYPE max_elem = data[first];
+		for (int i = first; i < last; i++)
+			if (max_elem < data[i])
+				max_elem = data[i];
+		return max_elem;
+	}
+
+	TYPE* fillArray()
+	{
+		TYPE* result = new TYPE[N];
+		for (int i = 0; i < N; i++)
+		{
+			result[i] = data[first + i];
+		}
+		return result;
+	}
+
+	TYPE* bubbleSort()
+	{
+		for (int i = 0; i < N - 1; i++)
+		{
+			for (int j = 0; j < N - i - 1; j++)
+			{
+				if (result[j + 1] < result[j])
+				{
+					TYPE tmp = result[j + 1];
+					result[j + 1] = result[j];
+					result[j] = tmp;
+				}
+			}
+		}
+		return result;
+	}
+
+    TYPE* selectionSort()
+	{
+		for (int i = 0; i < N - 1; i++)
+		{
+			int minIndex = i;
+			for (int j = i + 1; j < N; j++)
+			{
+				if (result[minIndex] > result[j])
+					minIndex = j;
+			}
+			TYPE tmp = result[i];
+			result[i] = result[minIndex];
+			result[minIndex] = tmp;
+		}
+		return result;
+	}
+
+    TYPE* insertionSort()
+	{
+		for (int i = 0; i < N - 1; i++)
+		{
+			int j = i + 1;
+			TYPE tmp = result[j];
+			while (j > 0 && tmp < result[j - 1])
+			{
+				result[j] = result[j - 1];
+				j--;
+			}
+			result[j] = tmp;
+		}
+		return result;
+	}
+};
+
+//vypise pole
+void printArray(TYPE* pole, int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d\t", pole[i]);
+	}
+	printf("\n");
+}
+
+//spoji setrizena dve pole do tretiho
+TYPE* mergeArrays(TYPE* arr1, TYPE* arr2, int N1, int N2)
+{
+	TYPE* answer = new int[N1 + N2];
+	int i = 0, j = 0, k = 0;
+
+	while (i < N1 && j < N2)
+		answer[k++] = arr1[i] < arr2[j] ? arr1[i++] : arr2[j++];
+
+	while (i < N1)
+		answer[k++] = arr1[i++];
+
+	while (j < N2)
+		answer[k++] = arr2[j++];
+
+	return answer;
+}
+
+// Thread will search the largest element in array 
+// from element arg->from with length of arg->length.
+// Result will be stored to arg->max.
+void *my_thread( void *void_arg )
+{
+    task_part *ptr_task = (task_part*)void_arg;
+    printf("Thread %d started from %d to %d...\n", ptr_task->id, ptr_task->first, ptr_task->last);
+
+    //ptr_task->max = ptr_task->search_max();
+    //ptr_task->result = ptr_task->bubbleSort();
+    ptr_task->result = ptr_task->selectionSort();
+    //ptr_task->result = ptr_task->insertionSort();
+
+    //printf("Found maximum in thread %d is %d\n", ptr_task->id, ptr_task->max);
+    printf("\nSorted in thread %d:\n", ptr_task->id);
+    printArray(ptr_task->result, ptr_task->N);
+
+    return NULL;
+}
+
+// Time interval between two measurements
+int timeval_to_ms( timeval *before, timeval *after )
+{
+    timeval res;
+    timersub( after, before, &res );
+    return 1000 * res.tv_sec + res.tv_usec / 1000;
+}
+
+#define LENGTH_LIMIT 10000000
+
+int main(int na, char **arg)
+{
+    // The number of elements must be used as program argument
+    if (na != 2)
+    {
+        printf("Specify number of elements, at least %d.\n", LENGTH_LIMIT);
+        return 0;
+    }
+    int N = atoi(arg[1]);
+    /*if (N < LENGTH_LIMIT)
+    {
+        printf("The number of elements must be at least %d.\n", LENGTH_LIMIT);
+        return 0;
+    }*/
+
+    // array allocation
+    TYPE *pole = new TYPE[N];
+    if (!pole)
+    {
+        printf("Not enought memory for array!\n");
+        return 1;
+    }
+
+    // Initialization of random number generator
+    srand((int)time(NULL));
+
+    printf("Random numbers generetion started...");
+    for (int i = 0; i < N; i++)
+    {
+        pole[i] = rand() % (N * 10);
+        if (!(i % LENGTH_LIMIT))
+        {
+            printf(".");
+            fflush(stdout);
+        }
+    }
+    printArray(pole, N);
+
+    printf("\nSort using 4 threads...\n");
+    pthread_t pt1, pt2, pt3, pt4;
+    task_part tp1(1, 0, N / 4, pole);
+    task_part tp2(2, N / 4, (N / 4) * 2, pole);
+    task_part tp3(3, (N / 4) * 2, (N / 4) * 3, pole);
+    task_part tp4(4, (N / 4) * 3, (N / 4) * 4, pole);
+    timeval time_before, time_after;
+
+    // Time recording before searching
+    gettimeofday(&time_before, NULL);
+
+    // Threads starting
+    pthread_create(&pt1, NULL, my_thread, &tp1);
+    pthread_create(&pt2, NULL, my_thread, &tp2);
+    pthread_create(&pt3, NULL, my_thread, &tp3);
+    pthread_create(&pt4, NULL, my_thread, &tp4);
+
+    // Waiting for threads completion
+    pthread_join(pt1, NULL);
+    pthread_join(pt2, NULL);
+    pthread_join(pt3, NULL);
+    pthread_join(pt4, NULL);
+
+    // Time recording after searching
+    gettimeofday(&time_after, NULL);
+
+    TYPE* sorted1 = mergeArrays(tp1.result, tp2.result, tp1.N, tp2.N);
+    TYPE* sorted2 = mergeArrays(tp3.result, tp4.result, tp3.N, tp4.N);
+    pole = mergeArrays(sorted1, sorted2, tp1.N + tp2.N, tp3.N + tp4.N);
+    printf("Result:\n");
+    printArray(pole, N);
+    printf("The search time: %d [ms]\n", timeval_to_ms(&time_before, &time_after));
+
+    /*printf("\nMaximum number search using two threads...\n");
+    pthread_t pt1, pt2;
+    task_part tp1(1, 0, N / 2, pole);
+    task_part tp2(2, N / 2, N, pole);
+    timeval time_before, time_after;
+
+    // Time recording before searching
+    gettimeofday(&time_before, NULL);
+
+    // Threads starting
+    pthread_create(&pt1, NULL, my_thread, &tp1);
+    pthread_create(&pt2, NULL, my_thread, &tp2);
+
+    // Waiting for threads completion 
+    pthread_join(pt1, NULL);
+    pthread_join(pt2, NULL);
+
+    // Time recording after searching
+    gettimeofday(&time_after, NULL);
+
+    printf("The found maximum: %d\n", MAX(tp1.get_max(), tp2.get_max()));
+    printf("The search time: %d [ms]\n", timeval_to_ms(&time_before, &time_after));
+
+    printf("\nMaximum number search using one thread...\n");
+
+    gettimeofday(&time_before, NULL);
+
+    // Searching in single thread
+    task_part single(333, 0, N, pole);
+    TYPE res = single.search_max();
+
+    gettimeofday(&time_after, NULL);
+
+    printf("The found maximum: %d\n", res);
+    printf("The search time: %d [ms]\n", timeval_to_ms(&time_before, &time_after));*/
+}
--- Threads/tridici algoritmy/tridici algoritmy/tridici algoritmy.cpp
+++ Threads/tridici algoritmy/tridici algoritmy/tridici algoritmy.cpp
@@ -1,82 +1,244 @@
 #include <iostream>
 #include <time.h>
 
+#define TYPE int
 
-/**
- * Buble sort (od nejmensiho)
- * @param array pole k serazeni
- * @param from od ktereho indexu
- * @param length kolik indexu
- */
-template <class T>
-void bubbleSort(T* array, int from, int length)
+using namespace std;
+
+class task_part
 {
-	for (int i = 0; i < length - 1; i++)
+public:
+	int id;                 // user identification
+	int first, last, N;     // data range
+	TYPE* data;             // array
+	TYPE max = 0;           // result
+	TYPE* result;   	    // result*
+
+	task_part(int myid, int first, int last, TYPE* ptr)
 	{
-		for (int j = 0; j < length - i - 1; j++)
+		this->id = myid;
+		this->first = first;
+		this->last = last;
+		this->data = ptr;
+		this->N = last - first;
+		this->result = fillArray();
+	}
+
+	TYPE get_max() { return max; }
+
+	TYPE* get_result() { return result; }
+
+	// function search_max search the largest number in part of array
+	// from the left (included) up to the right element
+	TYPE search_max()
+	{
+		TYPE max_elem = data[first];
+		for (int i = first; i < last; i++)
+			if (max_elem < data[i])
+				max_elem = data[i];
+		return max_elem;
+	}
+
+	TYPE* fillArray()
+	{
+		TYPE* result = new TYPE[N];
+		for (int i = 0; i < N; i++)
 		{
-			if (array[from + j + 1] < array[from + j])
+			result[i] = data[first + i];
+		}
+		return result;
+	}
+
+	TYPE* bubbleSort()
+	{
+		for (int i = 0; i < N - 1; i++)
+		{
+			for (int j = 0; j < N - i - 1; j++)
 			{
-				T tmp = array[from + j + 1];
-				array[from + j + 1] = array[from + j];
-				array[from + j] = tmp;
+				if (result[j + 1] < result[j])
+				{
+					TYPE tmp = result[j + 1];
+					result[j + 1] = result[j];
+					result[j] = tmp;
+				}
+			}
+		}
+		return result;
+	}
+
+	TYPE* selectionSort()
+	{
+		for (int i = 0; i < N - 1; i++)
+		{
+			int minIndex = i;
+			for (int j = i + 1; j < N; j++)
+			{
+				if (result[minIndex] > result[j])
+					minIndex = j;
+			}
+			TYPE tmp = result[i];
+			result[i] = result[minIndex];
+			result[minIndex] = tmp;
+		}
+		return result;
+	}
+
+	TYPE* insertionSort()
+	{
+		for (int i = 0; i < N - 1; i++)
+		{
+			int j = i + 1;
+			TYPE tmp = result[j];
+			while (j > 0 && tmp < result[j - 1])
+			{
+				result[j] = result[j - 1];
+				j--;
+			}
+			result[j] = tmp;
+		}
+		return result;
+	}
+};
+
+//vypise pole
+void printArray(TYPE* pole, int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << i << ":" << '\t';
+	}
+	cout << endl;
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d\t", pole[i]);
+	}
+	cout << endl;
+	cout << endl;
+}
+
+TYPE* fillArray(TYPE* array, int first, int last, int length)
+{
+	TYPE* result = new TYPE[length];
+	for (int i = 0; i < length; i++)
+	{
+		result[i] = array[first + i];
+	}
+	return result;
+}
+
+TYPE* bubbleSort(TYPE* array, int first, int last)
+{
+	int N = last - first;
+	TYPE* result = fillArray(array, first, last, N);
+
+	for (int i = 0; i < N - 1; i++)
+	{
+		for (int j = 0; j < N - i - 1; j++)
+		{
+			if (result[j + 1] < result[j])
+			{
+				TYPE tmp = result[j + 1];
+				result[j + 1] = result[j];
+				result[j] = tmp;
 			}
 		}
 	}
+	return result;
 }
 
-void selectSort(int* pole, int N)
+TYPE* selectionSort(TYPE* array, int first, int last)
 {
+	int N = last - first;
+	TYPE* result = fillArray(array, first, last, N);
+
 	for (int i = 0; i < N - 1; i++)
 	{
 		int minIndex = i;
-
 		for (int j = i + 1; j < N; j++)
 		{
-			if (pole[minIndex] > pole[j])
-			{
+			if (result[minIndex] > result[j])
 				minIndex = j;
-			}
 		}
-		std::swap(pole[minIndex], pole[i]);
+		TYPE tmp = result[i];
+		result[i] = result[minIndex];
+		result[minIndex] = tmp;
 	}
+	return result;
 }
 
-template <class T>
-void print(T* pole, int N)
+TYPE* insertionSort(TYPE* array, int first, int last)
 {
-	for (int i = 0; i < N; i++)
-	{
-		std::cout << i << ":" << '\t';
-	}
-	std::cout << std::endl;
+	int N = last - first;
+	TYPE* result = fillArray(array, first, last, N);
 
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < N - 1; i++)
 	{
-		std::cout << pole[i] << '\t';
+		int j = i + 1;
+		TYPE tmp = result[j];
+		while (j > 0 && tmp < result[j - 1])
+		{
+			result[j] = result[j - 1];
+			j--;
+		}
+		result[j] = tmp;
 	}
-	std::cout << std::endl;
-	std::cout << std::endl;
+	return result;
+}
+
+//spoji setrizena dve pole do tretiho
+TYPE* mergeArrays(TYPE* arr1, TYPE* arr2, int N1, int N2)
+{
+	TYPE* answer = new int[N1 + N2];
+	int i = 0, j = 0, k = 0;
+
+	while (i < N1 && j < N2)
+		answer[k++] = arr1[i] < arr2[j] ? arr1[i++] : arr2[j++];
+
+	while (i < N1)
+		answer[k++] = arr1[i++];
+
+	while (j < N2)
+		answer[k++] = arr2[j++];
+
+	return answer;
 }
 
 int main(void)
 {
-	const int N = 10;
-	char* pole = new char[N];
-	//double pole[N] = { 9.2, 4.5, 4.2, 2.3, 0.1 };
+	const int N = 1000000;
+	TYPE* pole = new TYPE[N];
+	//TYPE pole[N] = { 9.2, 4.5, 4.2, 2.3, 0.1, 0.002, 9.9, 2.31, 15.3, 11.12 };
 
 	srand((unsigned int)time(nullptr));
 
 	for (int i = 0; i < N; i++)
 	{
-		pole[i] = 33 + rand() % 93;
+		pole[i] = rand() % 100;
 	}
 
-	print(pole, N);
-	bubbleSort(pole, 0, N / 2);
-	print(pole, N);
-	bubbleSort(pole, N / 2, N - (N / 2));
-	print(pole, N);
+	/*int pole1[] = { 1, 3, 5, 7, 9 };
+	int pole2[] = { 2, 4, 8, 10, 11, 12, 16 };
+	printArray(pole1, 5);
+	printArray(pole2, 7);
+	int* pole3 = mergeArrays(pole1, pole2, 5, 7);
+	printArray(pole3, 5 + 7);*/
+
+	/*printArray(pole, N);
+	TYPE* sorted1 = bubbleSort(pole, 0, N / 2);
+	TYPE* sorted2 = bubbleSort(pole, N / 2, N);
+	printArray(sorted1, N / 2);
+	printArray(sorted2, N / 2);
+	TYPE* sorted_final = mergeArrays(sorted1, sorted2, N / 2, N / 2);
+	printArray(sorted_final, N);*/
+
+	//printArray(pole, N);
+	TYPE* sorted1 = insertionSort(pole, 0, N / 2);
+	TYPE* sorted2 = insertionSort(pole, N / 2, N);
+	//printArray(sorted1, N / 2);
+	//printArray(sorted2, N / 2);
+	TYPE* sorted_final = mergeArrays(sorted1, sorted2, N / 2, N / 2);
+	//printArray(sorted_final, N);
 
 	return 0;
 }
